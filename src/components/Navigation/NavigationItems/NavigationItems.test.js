62eacec6e89501eb06e23ecc33a064953a717799
Wenjie Chen <wenjiechen@WenjiedeMacBook-Air.local>
2020-10-07 17:46:33 -0400

    unit test example: navigation items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,15 @@
+// import React from 'react'; 
+
+// import { configure, shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// import NavigationItems from './NavigationItems';
+
+// // connect react with enzyme
+// configure({adapter: new Adapter()});
+
+// describe('<NavigationItems />', ()=> {
+//     it('should render two navigation item without authentication', () => {
+//         shallow(<NavigationItems />);
+//     });
+// });
