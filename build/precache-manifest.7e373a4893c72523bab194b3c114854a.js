f7e068762d1626bbace54bfa52bbd9039a25277c
Wenjie Chen <wenjiechen@WenjiedeMacBook-Air.local>
2020-10-21 22:13:20 -0400

    add gif images to repository media

---
+++ build/precache-manifest.7e373a4893c72523bab194b3c114854a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561f742cb323b4fd23ee9d7032e00beb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e74f2f1f330a8e5243a",
+    "url": "/static/css/main.a6e2c56d.chunk.css"
+  },
+  {
+    "revision": "3833e4d64306fa3b15eb",
+    "url": "/static/js/2.d7f56d07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d7f56d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e74f2f1f330a8e5243a",
+    "url": "/static/js/main.5c1feea5.chunk.js"
+  },
+  {
+    "revision": "c21c0f38b54bded085e4",
+    "url": "/static/js/runtime-main.39ed0be3.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
